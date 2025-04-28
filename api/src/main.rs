275ac7a24f9017c8c365a7a8fed6fb3037065b70
Anirudh Konidala <kidskodingclub@gmail.com>
2025-04-28 17:45:24 -0500

    added cargo project structure

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Router::new()
+        .route("/", get(|| async { 
+            "Hello, World!"
+        }));
+
+    println!("serving api on localhost:3000");
+
+    let listener = tokio::net::TcpListener::bind("localhost:3000")
+        .await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
